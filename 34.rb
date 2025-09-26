cdd4e918954bab7dc9f8fb41b505e2c302530f4f
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-09-27 08:50:59 +0900

    34

---
+++ 34.rb
@@ -0,0 +1,11 @@
+def close_far(a,b,c)
+  if ((a-b).abs == 1 || (a-c).abs == 1) &&(b-c).abs >= 2
+    puts "True"
+  else
+    puts "False"
+  end
+end
+
+close_far(1, 2, 10)
+close_far(1, 2, 3)
+close_far(4, 1, 3)
