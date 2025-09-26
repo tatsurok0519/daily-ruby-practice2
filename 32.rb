0a8332e5c315f718b3ab95029f0c045bc7b5e7c2
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-09-26 16:36:34 +0900

    32

---
+++ 32.rb
@@ -0,0 +1,16 @@
+def xyz_there(str)
+  if  str.include?(".xyz")
+    puts "False"
+  elsif str.include?("xyz")
+    puts "True"
+  else
+    puts "False"
+  end
+end
+
+# 呼び出し例
+xyz_there('abcxyz')
+xyz_there('abc.xyz')
+xyz_there('xyz.abc')
+xyz_there('azbycx')
+xyz_there('a.zbycx')
