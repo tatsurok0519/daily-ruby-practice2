8bd9c0cbbcbf4cc5b2c607ed11dd978113bffac5
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-10-04 06:50:17 +0900

    28-2

---
+++ 28-2.rb
@@ -0,0 +1,14 @@
+def withdraw(balance, amount)
+  fee = 110
+  total = amount + fee
+  if total < balance
+    puts "#{amount}円引き落としました。残高は#{balance - total}円です"
+  else
+    puts "残高不足です"
+  end
+end
+
+balance = 100000
+puts "いくら引き落としますか？（手数料110円かかります）"
+amount = gets.to_i
+withdraw(balance, amount)
