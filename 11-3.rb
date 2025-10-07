823f3865bf8e28217be423013abfc465ce4243c2
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-10-08 08:51:31 +0900

    12-3

---
+++ 11-3.rb
@@ -0,0 +1,12 @@
+def in1to10(num, outside_mode)
+  if outside_mode || (num >= 1 && num <= 10)
+    puts "True"
+  else
+    puts "False"
+  end
+end
+
+# 呼び出し例
+in1to10(5,false)
+in1to10(11,false) 
+in1to10(11,true)
