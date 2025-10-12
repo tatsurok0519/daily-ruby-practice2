8b773fdb9be371d5796b768c4de89b743af70fd0
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-10-13 05:47:24 +0900

    31-3

---
+++ 31-3.rb
@@ -0,0 +1,17 @@
+def near_ten(num)
+  hundreds = num / 100
+  tens = (num / 10) % 10
+  ones = num % 10
+  total = hundreds + tens + ones
+  if total <= 2 || total >= 8
+    puts "True"
+  elsif total <= 5
+    puts "10の倍数との差は#{total}です"
+  else
+    puts "10の倍数との差は#{10-total}です"
+  end
+end
+
+near_ten(117)
+near_ten(123)
+near_ten(111)
