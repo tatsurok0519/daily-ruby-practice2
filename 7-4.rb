b7a6559e46a84e88ae8806bb1e0c05177eca4487
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-10-16 05:35:25 +0900

    7-4

---
+++ 7-4.rb
@@ -0,0 +1,16 @@
+def search(target_num, input)
+  input.each_with_index do |target,i|
+    if target_num == target
+      puts "#{i+1}番目にあります"
+      return
+    end
+  end
+  puts "その数は含まれていません"
+end
+
+input = [3, 5, 9 ,12, 15, 21, 29, 35, 42, 51, 62, 78, 81, 87, 92, 93]
+# 呼び出し例
+search(11, input)
+search(5, input)
+search(12, input)
+search(7, input)
