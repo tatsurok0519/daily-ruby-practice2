6cacda6c3f46ca109ea9dc7b6db191fd73e2518a
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-10-16 05:58:48 +0900

    10-5

---
+++ 10-5.rb
@@ -0,0 +1,11 @@
+fruits_price = [["apple", [200, 250, 220]],
+ ["orange", [100, 120, 80]],
+ ["melon", [1200, 1500]]]
+
+def fruits_price(fruits)
+  fruits.each do |name,price|
+    puts "#{name}の合計金額は#{price.sum}円です"
+  end
+end
+
+fruits_price(fruits_price)
