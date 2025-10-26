d77a8ab471dbfa753d266347123ad2ec5593a045
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-10-26 14:19:33 +0900

    2-5

---
+++ 2-5.rb
@@ -0,0 +1,9 @@
+user_data = [
+ {user: {profile: {name: 'George'}}},
+ {user: {profile: {name: 'Alice'}}},
+ {user: {profile: {name: 'Taro'}}},
+]
+
+user_data.each do |data|
+  puts data[:user][:profile][:name]
+end
