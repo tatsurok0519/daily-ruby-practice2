5270698d2ea5dcaa80a34b7f3862ad1cf622d839
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-11-02 06:45:34 +0900

    38-3

--- 38.rb
+++ 38.rb
@@ -6,6 +6,12 @@ class Article
     @content = content
   end
 
+  def display
+    puts "著者: #{@author}"
+    puts "タイトル: #{@title}"
+    puts "本文: #{@content}"
+  end
+
 end
 
 article = Article.new("阿部", "Rubyの素晴らしさについて", "Awesome Ruby!")
