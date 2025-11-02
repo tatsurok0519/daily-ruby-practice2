e2c2850e009c86c3c2294d50ad7ff09890341246
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-11-03 04:21:38 +0900

    43-6

--- 43.rb
+++ 43.rb
@@ -3,6 +3,9 @@ def check_name(str)
     puts "!エラー!記号は登録できません"
   elsif str.include?(" ")
     puts "!エラー!空白は登録できません"
+  else
+    puts "登録が完了しました"
+  end
 end
 
 puts "登録したい名前を入力してください(例)YamadaTaro"
