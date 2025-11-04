179b96971a762737ce6f2b02d548c1e34c7d9f61
tatsurok0519 <arrivederci0519@yahoo.co.jp>
2025-11-05 08:16:05 +0900

    48-2

--- 48.rb
+++ 48.rb
@@ -2,5 +2,7 @@ def missing_char(str, n)
   # 処理を記述
 end
 
-# 呼び出し例（1つ目の引数には対象となる文字列を、2つ目の引数には対象となる文字列から何番目の文字を削除するのかを指定しましょう）
+
 missing_char('kitten', 1)
+missing_char('kitten', 2)
+missing_char('kitten', 4)
